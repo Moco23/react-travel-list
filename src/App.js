f93ca82a103a5756801087590b5e5121f96b7d98
Momcilo Nikolic <momcilo.pos@gmail.com>
2024-10-12 17:45:00 +0200

    start use lift up state and render new item to the ui

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: " Passports", quantity: 2, packed: false },
-  { id: 2, description: " Socks", quantity: 12, packed: true },
-  { id: 3, description: " Charger", quantity: 12, packed: false },
-  { id: 4, description: " Mobile", quantity: 12, packed: true },
-  { id: 5, description: " Laptop", quantity: 12, packed: false },
-  { id: 6, description: " food", quantity: 12, packed: true },
-];
-
 export default function App() {
+  // lift up for display new item in packin glist
+  const [items, setItems] = useState([]);
+
+  // handle add items function
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -24,7 +23,7 @@ function Logo() {
   return <h1>🏝️ Far Awey 🏝️</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   // set controls for elements
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
@@ -33,6 +32,24 @@ function Form() {
   function handleSubmit(e) {
     // desible auto reloading after form submission
     e.preventDefault();
+
+    // check description
+    if (!description) return;
+
+    // new item
+    const newItem = {
+      description,
+      quantity,
+      packed: false,
+      id: Date.now(),
+    };
+
+    // new item
+    onAddItems(newItem);
+
+    // set to initial state
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
@@ -63,11 +80,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
@@ -79,8 +96,7 @@ function Item({ item }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
-        {item.quantity}
-        {item.description}
+        {item.quantity} {item.description}
       </span>
       <button>❌</button>
     </li>
